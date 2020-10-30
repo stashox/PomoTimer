8481f5051c96e648478107bfbd9cb313fe768ff4
Pedro Franco <contatopedro@outlook.com>
2020-10-29 21:52:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
+        navigator.serviceWorker.register('PomoTimer/sw.js')
         .then((reg) => {
             console.log('Service Worker registrado', reg);
         }).catch((err) => {
@@ -12,29 +12,29 @@ if('serviceWorker' in navigator){
 const cacheName = "v1";
 
 const resourcesToPrecache = [
-    '/index.html',
+    'PomoTimer/index.html',
     '/',
-    '/apple-touch-icon.png',
-    '/apple-touch-icon.png',
-    '/arrow-back.svg',
-    '/logo.png',
-    '/logo2.png',
-    '/logo3.png',
-    '/logo4.png',
-    '/pause-btn.svg',
-    '/tuto1.png',
-    '/tuto2.png',
-    '/play-btn.svg',
-    '/restart-btn.svg',
-    '/sound.wav',
-    '/estilomobile.css',
-    '/estilomobile2.css',
-    '/estilo.css',
-    '/bootstrap.min.css',
-    '/main.js',
-    '/script.js',
-    '/bootstrap.min.js',
-    '/jquery-3.5.1.slim.min.js',
+    'PomoTimer/apple-touch-icon.png',
+    'PomoTimer/apple-touch-icon.png',
+    'PomoTimer/arrow-back.svg',
+    'PomoTimer/logo.png',
+    'PomoTimer/logo2.png',
+    'PomoTimer/logo3.png',
+    'PomoTimer/logo4.png',
+    'PomoTimer/pause-btn.svg',
+    'PomoTimer/tuto1.png',
+    'PomoTimer/tuto2.png',
+    'PomoTimer/play-btn.svg',
+    'PomoTimer/restart-btn.svg',
+    'PomoTimer/sound.wav',
+    'PomoTimer/estilomobile.css',
+    'PomoTimer/estilomobile2.css',
+    'PomoTimer/estilo.css',
+    'PomoTimer/bootstrap.min.css',
+    'PomoTimer/main.js',
+    'PomoTimer/script.js',
+    'PomoTimer/bootstrap.min.js',
+    'PomoTimer/jquery-3.5.1.slim.min.js',
 ];
 
 self.addEventListener('install', (event) => {
