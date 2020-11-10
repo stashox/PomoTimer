38e4dac01af07337a33699dfaa5b32e72c74b559
Pedro Franco <contatopedro@outlook.com>
2020-11-10 01:57:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
+        navigator.serviceWorker.register('/sw.js')
         .then((reg) => {
             console.log('Service Worker registrado', reg);
         }).catch((err) => {
@@ -13,28 +13,28 @@ const cacheName = "v2.22";
 
 const resourcesToPrecache = [
     '/',
-    './assets/img/apple-touch-icon.png',
-    './assets/img/arrow-back.svg',
-    './assets/img/logo.png',
-    './assets/img/logo2.png',
-    './assets/img/logo3.png',
-    './assets/img/logo4.png',
-    './assets/img/pause-btn.svg',
-    './assets/img/tuto1.png',
-    './assets/img/tuto2.png',
-    './assets/img/play-btn.svg',
-    './assets/img/restart-btn.svg',
-    './assets/img/github.svg',
-    './assets/img/insta.svg',
-    './assets/img/tracos.svg',
-    './assets/sound.wav',
-    './assets/css/bootstrap.min.css',
-    './assets/js/main.js',
-    './assets/js/script.js',
-    './assets/js/bootstrap.min.js',
-    './assets/js/jquery-3.5.1.slim.min.js',
-    './index.html',
-    './assets/css/estilo.css',
+    '/assets/img/apple-touch-icon.png',
+    '/assets/img/arrow-back.svg',
+    '/assets/img/logo.png',
+    '/assets/img/logo2.png',
+    '/assets/img/logo3.png',
+    '/assets/img/logo4.png',
+    '/assets/img/pause-btn.svg',
+    '/assets/img/tuto1.png',
+    '/assets/img/tuto2.png',
+    '/assets/img/play-btn.svg',
+    '/assets/img/restart-btn.svg',
+    '/assets/img/github.svg',
+    '/assets/img/insta.svg',
+    '/assets/img/tracos.svg',
+    '/assets/sound.wav',
+    '/assets/css/bootstrap.min.css',
+    '/assets/js/main.js',
+    '/assets/js/script.js',
+    '/assets/js/bootstrap.min.js',
+    '/assets/js/jquery-3.5.1.slim.min.js',
+    '/index.html',
+    '/assets/css/estilo.css',
 ];
 
 self.addEventListener('install', (event) => {
